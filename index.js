a694a7e094104f1677c72b7c80751eeab7e7da47
An Phi <aphi@skidmore.edu>
2022-10-16 15:48:51 -0400

    create repo

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
+import { axios } from "axios";
 
-const PORT = 9999;
-
-const server = fastify({
-  logger: true,
-});
-
-server.register(fastifyCors, {
-  methods: ["OPTIONS"],
-  origin: [/localhost/],
-  credentials: true,
-});
-
-server.listen(
-  {
-    port: PORT,
-  },
-  (error, address) => {
-    if (error) {
-      throw error;
-    }
-  }
-);
+console.log("");
