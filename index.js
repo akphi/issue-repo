d14655f38a5f14818379d394ecc9f6167b2915cd
An Phi <aphi@skidmore.edu>
2022-10-24 09:51:30 -0400

    minor

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { axios } from "axios";
 
-console.log("");
+await axios.post(
+  `http://localhost:6300/pure/v1/grammar/grammarToJson/model`,
+  "",
+  {
+    // headers: {
+    //   [HttpHeader.CONTENT_TYPE]: ContentType.TEXT_PLAIN,
+    // },
+    // params: {
+    //   returnSourceInformation: false,
+    // },
+  }
+);
